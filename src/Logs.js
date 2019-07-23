5f64d0372ff7d0a907ae2e566f2df12b52d13f33
zelol <slf.pro@gmail.com>
2019-07-23 21:40:34 +0200

    display time for each message

--- src/Logs.js
+++ src/Logs.js
@@ -4,7 +4,12 @@ const Logs = ({ messages }) => {
 
   return (
     <div>
-      {messages.map((message) => <li key={message._id}><span>{message.date}</span>{message.text}</li>)}
+      {messages.map((message) => {
+        let date = new Date(message.date)
+        date = date.getHours() + ":" + date.getMinutes() + ' '
+
+        return <li key={message._id}><span>{date}</span>{message.text}</li>
+      })}
     </div>
   )
 }

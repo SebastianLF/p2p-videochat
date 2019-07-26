ca530d260cbf231af12263753788766a34ddf1ca
zelol <slf.pro@gmail.com>
2019-07-26 17:26:54 +0200

    add styling to chat

--- src/Logs.js
+++ src/Logs.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { getUsername } from './localStorage.js'
 
 const Logs = ({ messages }) => {
+
+  const isOwner = (sender) => sender === getUsername('username')
 
   return (
     <div>
       {messages.map((message) => {
-        let date = new Date(message.date)
-        date = date.getHours() + ":" + date.getMinutes() + ' '
+        // let date = new Date(message.date)
+        // date = date.getHours() + ":" + date.getMinutes() + ' '
+        const myStyle = {
+          textAlign: "right",
+          maxWidth: "300px"
+        }
 
-        return <li key={message._id}><span>{date}</span>{message.text}</li>
+        const hisStyle = {
+          textAlign: "left",
+          color: 'red'
+        }
+
+        return (
+          <div style={ isOwner(message.sender) ? myStyle : hisStyle } key={message._id}>
+            <h4> {isOwner(message.sender) ? 'Vous avez dit' : `${message.sender} a dit:`} </h4>
+            <p>{message.text}</p>
+          </div>
+        )
       })}
     </div>
   )

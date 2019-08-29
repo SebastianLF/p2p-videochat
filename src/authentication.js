ea8c45f07e2950e8604d3a0537d0db95424ed62c
zelol <slf.pro@gmail.com>
2019-08-30 00:34:13 +0200

    add a private auth property inside module

--- src/authentication.js
+++ src/authentication.js
@@ -6,8 +6,9 @@ const generateUniqueUsername = () => {
 }
 
 const key = 'user'
+const isAuth = false
 
-function storageAvailable (type = 'localStorage') {
+function storageAvailable(type = 'localStorage') {
   const storage = window[type]
   try {
     var x = '__storage_test__'
@@ -16,23 +17,23 @@ function storageAvailable (type = 'localStorage') {
     return true
   } catch (e) {
     return e instanceof window.DOMException && (
-    // everything except Firefox
+      // everything except Firefox
       e.code === 22 ||
-          // Firefox
-          e.code === 1014 ||
-          // test name field too, because code might not be present
-          // everything except Firefox
-          e.name === 'QuotaExceededError' ||
-          // Firefox
-          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-          // acknowledge QuotaExceededError only if there's something already stored
-          (storage && storage.length !== 0)
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      (storage && storage.length !== 0)
   }
 }
 
 export const saveUsername = (username) => {
   if (storageAvailable()) {
-    window.localStorage.setItem('user', JSON.stringify(username))
+    window.localStorage.setItem(key, JSON.stringify(username))
   }
 }
 
@@ -44,10 +45,4 @@ export const getUsername = () => {
   return JSON.parse(window.localStorage.getItem(key))
 }
 
-export const isAuthenticated = () => {
-  if (getUsername()) {
-    return true
-  }
-
-  return false
-}
+export const isAuthed = () => isAuth

9d60a1885d0274c40ebb69ead3cabf8e12ae3118
zelol <slf.pro@gmail.com>
2019-09-08 20:43:25 +0200

    refactor Room component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import socket from '../socket.js'
 import ConversationBox from './ConversationBox'
 import SidePanel from './SidePanel'
-import CONSTANTS from '../constants.js'
 import './Room.css'
 import NoMatch from './NoMatch'
 import Loader from './Loader'
-import { getUsername } from '../authentication.js'
+import { isAuthed } from '../authentication'
+import { API_URL } from '../constants'
 
-class Room extends React.Component {
+function Room(props) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isValid, setIsValid] = useState(false)
+  const [participants, setParticipants] = useState([])
+  const getRoomData = (roomId) => axios.get(`${API_URL}/rooms/${roomId}`)
+
+  useEffect(() => {
+    setTimeout(() =>
+      getRoomData(props.match.params.roomId)
+        .then(({ data }) => {
+          setIsLoading(false)
+          setIsValid(true)
+        })
+        .catch(e => {
+          setIsLoading(false)
+          setIsValid(false)
+        })
+      , 1000)
+  }, [props.match.params.roomId])
+
+  if (isLoading) return <Loader />
+
+  if (!isValid) return <NoMatch {...props} />
+
+  return (
+    <div className='room'>
+      <SidePanel participants={participants} />
+      <ConversationBox roomId={props.match.params.roomId} />
+    </div>
+  )
+}
+
+/* class Room extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -59,11 +91,9 @@ class Room extends React.Component {
 
     return (
       <div className='room'>
-        <SidePanel participants={this.state.room.participants} />
-        <ConversationBox roomName={this.props.match.params.roomId} />
       </div>
     )
   }
 }
-
+ */
 export default Room
